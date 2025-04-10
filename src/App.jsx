91c90b956f410473c74a0d7179c225492c7033df
Fertanilo <basf080605mmcxlra8@lake.edu.mx>
2025-04-10 09:59:53 -0600

    Se agrego la infopapu

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   return (
     <>
      <h1>Pokedex</h1>
+     <h2>Welcome to the pokedex</h2>
+     <p>Here you can find information about your favorite Pokemon</p>
      <img src="" alt="" />
     </>
   )
